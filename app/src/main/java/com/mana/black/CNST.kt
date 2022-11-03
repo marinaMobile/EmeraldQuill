d296a246a923384e7d86a982f368d96da8c745ad
Marina <marina@Marinas-MacBook-Pro.local>
2022-11-03 13:33:52 +0300

    first commit

---
+++ app/src/main/java/com/mana/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.mana.black
+
+object CNST {
+    const val AF_DEV_KEY = "d6k7f2q55VgJR56KG5Btsd"
+    const val jsoupCheck = "4f6h"
+    const val ONESIGNAL_APP_ID = "69cb7b72-3f66-4875-bfeb-b0efd50cf8ad"
+
+    var lru = "http://luckyquill.xyz/go.php?to=1&"
+
+
+    val odone = "sub_id_1="
+    val twoSub = "sub_id_2="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+    var DEV: String = "false"
+}
